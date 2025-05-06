05a7f28d01d6a0189dfb3332bc70f4a49fe0b090
thund3r1331 <artemvasilchenko2009@gmail.com>
2025-05-06 17:42:11 +0500

    some shit again

---
+++ src/main/java/thund/learningbtw/SummonShadow.java
@@ -0,0 +1,21 @@
+package thund.learningbtw;
+
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.util.Vector;
+
+public class SummonShadow implements Listener {
+    public void spawnBehindPlayer(Player player) {
+        Vector vector = player.getLocation().getDirection().normalize().multiply(-5);
+        Location locationBehind = player.getLocation().add(vector);
+
+        ArmorStand armorStand = (ArmorStand) player.getWorld().spawnEntity(locationBehind, EntityType.ARMOR_STAND);
+
+        armorStand.setGravity(false);
+        armorStand.setCustomName("Shadow");
+    }
+}
+
--- src/main/java/thund/learningbtw/listeners/PlayerEvents.java
+++ src/main/java/thund/learningbtw/listeners/PlayerEvents.java
@@ -5,8 +5,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import thund.learningbtw.SummonShadow;
 
 import java.util.HashMap;
+
 
 public class PlayerEvents implements Listener {
     public static HashMap<Player, Integer> playerMap = new HashMap<>(); //integer - шанс спавна тени
@@ -21,6 +23,8 @@ public class PlayerEvents implements Listener {
     public void onDMGOtherEntity(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player player && playerMap.containsKey(player)) {
             playerMap.compute(player, (k, spawnChance) -> spawnChance + 1);
+
+            new SummonShadow().spawnBehindPlayer(player);
         }
     }
 }
